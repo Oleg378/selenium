0215fb20685e8329ee6d9e7dd0c49eb7fc8a64c4
Oleg <oleg-dmitriewich@yandex.ru>
2023-12-09 00:20:03 +0200

    an example of usage fixture + mark usage

--- fixture.py
+++ fixture.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture # you can use @pytest.fixture(scope="class") or "function", class", "module", "session"
+@pytest.fixture  # you can use @pytest.fixture(scope="class") or "function", class", "module", "session"
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
@@ -24,12 +24,22 @@ def prepare_data():
 
 class TestMainPage1():
     # вызываем фикстуру в тесте, передав ее как параметр
+
+    @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
+    @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
 
-# pytest -s -v fixture.py
+# pytest -s -v fixture.py pytest -s -v -m smoke fixture.py - you cag run only tests marked with specific name (you
+# need use '-m' mode and call required mark name, for example 'smoke')
+
+# aslo strictly recommended to create pytest.ini file:
+# [pytest]
+# markers =
+#     smoke: marker for smoke tests
+#     regression: marker for regression tests
